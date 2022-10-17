126c8ab6634792c0a7312cf1f0f3cd0de562d234
doktorjohan <johan.hollak@ut.ee>
2022-10-17 20:54:09 +0300

    improved element creation

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,42 @@
+window.onload = function () {
+    console.log("tere")
 
-console.log("tere")
+    fetch("http://myjson.dit.upm.es/api/bins/azjs")
+        .then(response => response.json())
+        .then(json => {
 
-fetch("http://myjson.dit.upm.es/api/bins/azjs")
-    .then(response => response.json())
-    .then(json => {
+            for (let post of json) {
 
-        for (let post of json) {
+                let postContainer = document.createElement("div")
+                postContainer.className = "post"
 
-            let postContainer = document.createElement("div")
-            postContainer.className = "post"
-            let title = document.createElement("h3")
-            let titleText = document.createTextNode(post.title)
-            title.appendChild(titleText)
+                let postHeader = document.createElement("div")
+                postHeader.className = "postHeader"
 
-            let postBody = document.createElement("p")
-            let postText = document.createTextNode(post.body)
-            postBody.appendChild(postText)
+                let postBodyContainer = document.createElement("div")
+                postBodyContainer.className = "postBody"
 
-            postContainer.appendChild(title)
-            postContainer.appendChild(postBody)
+                let postFooter = document.createElement("div")
+                postFooter.className = "postFooter"
 
-            let feed = document.getElementsByClassName("feed")[0]
-            feed.appendChild(postContainer)
-        }
+                let title = document.createElement("h3")
+                let titleText = document.createTextNode(post.title)
+                title.appendChild(titleText)
+                postHeader.appendChild(title)
+
+                let postBody = document.createElement("p")
+                let postText = document.createTextNode(post.post)
+                postBody.appendChild(postText)
+                postBodyContainer.appendChild(postBody)
+
+                postContainer.appendChild(postHeader)
+                postContainer.appendChild(postBody)
+                postContainer.appendChild(postFooter)
+
+                let feed = document.getElementById("feed")
+                feed.appendChild(postContainer)
+            }
 
 
-
-    })
+        })
+}
