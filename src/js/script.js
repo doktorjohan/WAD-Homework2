a49d2897e5c6ce6ac867e4e2b9124a128cfafded
Artur Kreegipuu <artur@kreegipuu.net>
2022-10-23 15:42:35 +0300

    script improvement for showing posts

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
     console.log("tere")
-
     fetch("http://myjson.dit.upm.es/api/bins/azjs")
         .then(response => response.json())
         .then(json => {
@@ -19,18 +18,47 @@ window.onload = function () {
                 let postFooter = document.createElement("div")
                 postFooter.className = "postFooter"
 
-                let title = document.createElement("h3")
+                /*let title = document.createElement("h3")
                 let titleText = document.createTextNode(post.title)
                 title.appendChild(titleText)
-                postHeader.appendChild(title)
+                postHeader.appendChild(title)*/
 
+                // Adding profile picture to post's header
+                let profilePic = document.createElement("img")
+                profilePic.src = "res/images/logo.png"
+                profilePic.alt = "logo"
+                profilePic.className = "profilePic"
+                postHeader.appendChild(profilePic)
+
+                // Adding date of the post to post's header
+                let time = document.createElement("p")
+                let postTime = document.createTextNode(post.createTime)
+                time.appendChild(postTime)
+                postHeader.appendChild(time)
+
+                // Adding post's image if provided in the json file
+                if (post.image != null){
+                    let postImg = document.createElement("img")
+                    postImg.src = post.image
+                    postImg.alt = post.image.split("/")[post.image.split("/").length-1]
+                    postImg.className = "postPic"
+                    postBodyContainer.appendChild(postImg)
+                }
+                // Adding post's text to post's body
                 let postBody = document.createElement("p")
                 let postText = document.createTextNode(post.post)
                 postBody.appendChild(postText)
                 postBodyContainer.appendChild(postBody)
 
+                // Adding like button to post's footer
+                let likeBtn = document.createElement("img")
+                likeBtn.src = "res/images/like.png"
+                likeBtn.alt = "like"
+                likeBtn.className = "like"
+                postFooter.appendChild(likeBtn)
+
                 postContainer.appendChild(postHeader)
-                postContainer.appendChild(postBody)
+                postContainer.appendChild(postBodyContainer)
                 postContainer.appendChild(postFooter)
 
                 let feed = document.getElementById("feed")
